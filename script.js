ca437e94c59a9701700030d4d3a413e2a35b2a3f
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2021-11-09 18:40:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function functionCalls() {
 }
 
 function checkWindowClick() {
+    
     terminalBody.addEventListener ('click', function(event){
         input.focus();
     });
@@ -25,6 +26,7 @@ function checkPressedEnter() {
 }
 
 function execute(inputValue) {
+    
     let temp = input.value;
     input.remove();
     terminalBody.innerHTML += temp;
@@ -41,6 +43,7 @@ function executeCommand(command){
         terminalBody.innerHTML += `${data[command][i].name}: ` ;
 
         if(data[command][i].value.includes('http')){
+            
             terminalBody.innerHTML += `<a href="${data[command][i].value}" target="_blank">${data[command][i].value}</a><br>` ;
         } else{
             terminalBody.innerHTML += `${data[command][i].value}<br>` ;
@@ -53,6 +56,7 @@ function checkCommand(inputCommand){
     let command = inputCommand.split(" ")[0];
 
     if(command){
+        
         if(command === 'clear'){ commandClear();}
         else if(command === 'echo') {commandEcho(inputCommand);}
         else if(command === 'github'){ commandGithub(command)}
