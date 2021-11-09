d64f5c9508ceea0b40f08702fae0c14e27635274
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2021-11-09 18:39:34 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+var data = {
+  
+    name : [
+      
+      {name: 'Name', value: 'Samridhi Saini'}
+      
+    ],
+  
+    bio: [
+      
+      {name: 'Name', value: 'Samridhi Saini'},
+      {name: 'Email', value: 'samridhisaini02002@gmail.com'},
+      {name: 'Phone', value: '+91-6397674278'},
+      {name: 'Country', value: 'India'},
+      {name: 'City', value: 'Delhi'}
+      
+    ],
+  
+    github: [
+      
+      {name: 'GitHub', value: 'SAMRIDHISAINII'}
+      
+    ],
+    
+    socials: [
+      
+      {name: 'LinkedIn', value: 'https://www.linkedin.com/in/samridhi-saini-b358841a6/'},
+      {name: 'GitHub', value: 'https://github.com/SAMRIDHISAINII'},
+      {name: 'Instagram', value: 'https://www.instagram.com/samridhisainii/'},
+      {name: 'Instagram(CodeArryena)', value: 'https://www.instagram.com/codearryena/'},
+      {name: 'Blogs', value: 'https://medium.com/@samridhisaini02002'},
+      {name: 'Youtube', value: 'https://www.youtube.com/channel/UCdSTb4QUpWyjqA0dOZzWqwA'},
+      
+    ]
+  }
+
